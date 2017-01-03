321e6bdfa05648e47a143bcfdd2bf6e6f43dc9dc
freedragon <1579529016@qq.com>
2017-01-03 15:34:04 +0800

    Initial Commit

---
+++ exercise_opengl/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  exercise_opengl
+//
+//  Created by freedragon on 03/01/2017.
+//  Copyright © 2017 freedragon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
