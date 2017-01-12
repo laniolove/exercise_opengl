8e4f5053525cc3fa8f1268034ccdd50dda88682f
freedragon <freedragon@lanio.local>
2017-01-13 02:32:33 +0800

    new move

--- exercise_opengl/main.cpp
+++ exercise_opengl/main.cpp
@@ -7,10 +7,10 @@
 //
 
 #include <iostream>
-
+//using namespace std;
 //Be sure to include GLEW before GLFW.
 //The includes(like GL/gl.h),
-//so include GLEW before other header files that require OpenGL does the thick.
+//so include GLEW before other heade`r files that require OpenGL does the thick.
 
 //GLEW
 #define GLEW_STATIC // using the static version of the GLEW library
@@ -19,18 +19,31 @@
 //GLFW
 #include "glfw3.h"
 /*
-glfwInit(): init the GLFW library, before most GLFW func can b used, The Func returns GL_TRUE if succesful,otherwise GL_FALSE is returned when an error.
-glfwWindowHint(int target, int hint): the func sets hints for the next call to glfwCreateWindow.the hints, once set, retain their values until changed by a another call to glfwWindowHint. the target param is with the GLFW_.
-
-
+glfwInit(): 初始化GLFW库, succesful：GL_TRUE，failed：GL_FALSE.
+glfwWindowHint(int target, int hint):the target param is with the GLFW_.改变这些设置的value，可通过glfwWindowHint／glfwDefaultWindowHints／直到这个库终止了。这个接口不能检测value是否有效；如果没效果，会在glfwCreateWindow时候报错（GLFW_NOT_INITIALIZED／GLFW_INVALID_ENUM）。设置这个要在主线程。glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE): 给macOS系统做兼容的。
+glfwCreateWindow: 
+ratina屏的macOS在设置viewport时，直接设置高宽会有尺寸问题，应该从缓冲获取窗口的宽高。
+获取视口:
+    int height, width;
+    glfwGetFramebufferSize(Window, &width, &heigt);
+    glViewport(0, 0, width, height);
 */
+
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-//    std::cout << "Hello, World!" <<std::endl;
+    //init GLFW
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    
+    //create window object
+    GLFWwindow* window = glfwCreateWindow(800, 600, "learn OpenGL", nullptr, nullptr);
+    if (window == nullptr) {
+        std::cout << "Failed to ceate GLFW window" << std::endl;
+        glfwTerminate();
+    }
     return 0;
 }
